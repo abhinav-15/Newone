a71e1bf06e461fe84ad87c255adff2c8fe0146b3
abhinav-15 <wwww.lucky1598@gmail.com>
2023-04-26 11:22:45 +0530

    first

---
+++ Book.java
@@ -0,0 +1,27 @@
+package Java_IO_Serialization_25_april_lab_session;
+/*
+ * create desktop  application for library mgt system
+
+define class book having fields like bname ,aname ,price and get method and display;
+
+define other methods like
+    writeToFile()
+  readfromFile()
+  searchfromFile()
+
+In main design menu driven application
+ 1) store data to file
+ 2) read data fom file
+ 3) search data from file
+ 4) exit
+ */
+public class Book {
+
+	String bname;
+	String aname;
+	void get()
+	{}
+	
+	void display() {}
+	
+}
