19088f7fc3df154767d7039aa80784e657d5aa52
abhinav-15 <wwww.lucky1598@gmail.com>
2023-05-04 21:37:18 +0530

    Initial commit

---
+++ asdm.java
@@ -0,0 +1,13 @@
+public class asdm {
+    public static void main(String[] args)
+    {
+    	int a=100;
+    	
+    	System.out.println(a>10 && a<20); //if all condition is true then true true
+    	
+    	System.out.println(a>10 || a>20); // if any one condition is true then true
+        
+    	System.out.println(a!=10);
+    	System.out.println( !(5==5));
+    }
+}
